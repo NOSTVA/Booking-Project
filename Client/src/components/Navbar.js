2247e4baac0d47286e94a65ff8420af8a0cc1a84
Lime <alsayyd.medo72@gmail.com>
2023-06-19 13:39:02 +0300

    update the login and register design

--- Client/src/components/Navbar.js
+++ Client/src/components/Navbar.js
@@ -30,7 +30,8 @@ export default function Navbar() {
       zIndex={99}
       borderBottom={1}
       borderStyle={"solid"}
-      borderColor={useColorModeValue("gray.300", "gray.600")}>
+      borderColor={useColorModeValue("gray.300", "gray.600")}
+    >
       <Flex
         bg={useColorModeValue("white", "gray.800")}
         color={useColorModeValue("gray.600", "white")}
@@ -39,7 +40,8 @@ export default function Navbar() {
         px={{ base: 4 }}
         align={"center"}
         maxWidth={1300}
-        mx="auto">
+        mx="auto"
+      >
         <Flex flex={{ base: 1 }} justify={{ base: "start" }}>
           <Logo />
           <Flex display={{ base: "none", md: "flex" }} ml={10}>
@@ -90,7 +92,8 @@ const DesktopNav = () => {
                   _hover={{
                     textDecoration: "none",
                     color: linkHoverColor,
-                  }}>
+                  }}
+                >
                   {navItem.label}
                 </Link>
               </PopoverTrigger>
@@ -107,7 +110,8 @@ const MobileNav = () => {
     <Stack
       bg={useColorModeValue("white", "gray.800")}
       p={4}
-      display={{ md: "none" }}>
+      display={{ md: "none" }}
+    >
       {NAV_ITEMS.map((navItem) => (
         <MobileNavItem key={navItem.label} {...navItem} />
       ))}
@@ -128,10 +132,12 @@ const MobileNavItem = ({ label, children, href }) => {
         align={"center"}
         _hover={{
           textDecoration: "none",
-        }}>
+        }}
+      >
         <Text
           fontWeight={600}
-          color={useColorModeValue("gray.600", "gray.200")}>
+          color={useColorModeValue("gray.600", "gray.200")}
+        >
           {label}
         </Text>
       </Flex>
@@ -143,7 +149,8 @@ const MobileNavItem = ({ label, children, href }) => {
           borderLeft={1}
           borderStyle={"solid"}
           borderColor={useColorModeValue("gray.200", "gray.700")}
-          align={"start"}></Stack>
+          align={"start"}
+        ></Stack>
       </Collapse>
     </Stack>
   );
@@ -155,7 +162,8 @@ const Logo = () => {
       as="b"
       textAlign={useBreakpointValue({ base: "center", md: "left" })}
       fontFamily={"heading"}
-      color={useColorModeValue("gray.800", "white")}>
+      color={useColorModeValue("gray.800", "white")}
+    >
       BOOKING
     </Text>
   );
@@ -170,5 +178,9 @@ const NAV_ITEMS = [
     label: "Dashboard",
     href: "/dashboard",
     adminOnly: true,
+  },
+  {
+    label: "Logout",
+    href: "/logout",
   },
 ];
